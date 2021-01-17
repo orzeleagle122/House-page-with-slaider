26e854e75bf31e7139a678fbf36cf63b5b40ad8b
Patryk Orłowski <orzeleagle122@gmail.com>
2021-01-17 14:23:49 +0100

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navbar} from './Navbar/Navbar';
-export {default as Button} from './Button/Button';
-export {default as Hero} from './Hero/Hero';
-export {default as DropDownMenu} from './DropDownMenu/DropDownMenu';
-export {default as InfoSection} from './InfoSection/InfoSection';
